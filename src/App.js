922effedaf220d9dac717376ec93a85deb67c3e9
charly <carlosgarita3110@gmail.com>
2022-07-02 16:23:59 -0600

    feat: Los props vienen de una JSON

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import './App.css';
 import Testimonio from './componentes/Testimonio';
+import data from './data.json';
+
 
 function App() {
+  console.log(data);
   return (
     <div className='App'>
       <div className='contenedor-principal'>
         <h1>Here is what our alumni say about freeCodeCamp:</h1>
         <Testimonio 
-          imagen='shawn'
-          nombre='Shawn Wang'
-          pais='Singapore'
-          trabajo='Software Engineer'
-          empresa='Amazon'
-          testimonio="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life." />
+          imagen = {data['user1'].imagen}
+          nombre = {data['user1'].nombre}
+          pais = {data['user1'].pais}
+          trabajo = {data['user1'].trabajo}
+          empresa = {data['user1'].empresa}
+          testimonio = {data['user1'].testimonio}
+        />
         <Testimonio 
-          imagen='sarah'
-          nombre='Sarah Chima'
-          pais='Nigeria'
-          trabajo='Software Engineer'
-          empresa='ChatDesk'
-          testimonio="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company." />
+          imagen = {data['user2'].imagen}
+          nombre = {data['user2'].nombre}
+          pais = {data['user2'].pais}
+          trabajo = {data['user2'].trabajo}
+          empresa = {data['user2'].empresa}
+          testimonio = {data['user2'].testimonio}
+        />
         <Testimonio
-          imagen='emma'
-          nombre='Emma Bostian'
-          pais='Sweeden'
-          trabajo='Software Engineer'
-          empresa='Spotify'
-          testimonio="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify." />
+          imagen = {data['user3'].imagen}
+          nombre = {data['user3'].nombre}
+          pais = {data['user3'].pais}
+          trabajo = {data['user3'].trabajo}
+          empresa = {data['user3'].empresa}
+          testimonio = {data['user3'].testimonio}
+        />
       </div>
     </div>
   );
